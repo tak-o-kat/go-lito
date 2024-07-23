c06944e12274791c04371ab00a69680c54fad5a0
takokat <danobaba@protonmail.com>
2024-07-22 22:00:18 -0700

    Refactor parse for clarity and add in helper functions

--- cmd/lito/parser.go
+++ cmd/lito/parser.go
@@ -1,97 +1,244 @@
 package lito
 
 import (
+	//"runtime"
 	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
-type Counters struct {
+type Totals struct {
 	blocksOnChain int
 	blocksProposed int
 	softVotes int
 	certVotes int
 }
 
-type Block struct {
-	block string
-	isOnChain bool
-	dateTime string	
+type Blocks struct {
+	// LastRound int `json:"last-round"`
+	Round uint64 `json:"round"`
+	TimeStamp string `json:"time"`
+	Sender string `json:"sender"`
+	isOnChain bool 
+	startTime time.Time
+	endTime time.Time
 }
 
+type Votes struct {
+	// LastRound int `json:"last-round"`
+	Round uint64 `json:"round"`
+	TimeStamp string `json:"time"`
+	Type int64 `json:"ObjectStep"`
+}
+
+type LogData struct {
+	totals Totals
+	Blocks map[uint64]Blocks
+	votes []Votes
+	round uint64
+	startTime time.Time
+	sender string
+}
+
+// func getAccountAddress() string {
+// 	cmd := "goal account partkeyinfo | " +
+// 			"sed -n '/Parent/p' | " +
+// 			"awk '{print $3}'"
+// 	stdout, err := exec.Command("bash", "-c", cmd).Output()
+
+// 	if err != nil {
+// 		fmt.Printf("%s", err)
+// 	}
+// 	return strings.TrimSuffix(string(stdout), "\n")
+// }
+
+
 func Parse(la *LitoApp) {
-	address := la.algodInfo.partAccount // CTTR2JYLHQVQRTLAMNUA475YSBBOPSUOG5E72AOR6MQYRNFVJX4Q5YZUMY
-	logFile := la.algodInfo.archiveFile
+	fmt.Println(la.algodInfo.archiveFile)
+	address := "VEJGTLTKNT3VGLG2GVB2LMXC55WYW6J6WPZ76XTY2Y46TRJQOORWERYXYE" //os.Getenv("ADDRESS")
+
+	// Hard code for now
+	logFile := os.Getenv("ALGORAND_DATA") + "/archive.log" // use 
 	file , ferr := os.Open (logFile)
 	if ferr != nil {
 			panic(ferr)
 	}
 
-	counters := Counters{
-		blocksOnChain: 0,
-		blocksProposed: 0,
-		softVotes: 0,
-		certVotes: 0,
+	var parsedData = LogData{
+		totals: Totals{},
+		Blocks: make(map[uint64]Blocks),
+		votes: []Votes{},
+		round: 0,
+		startTime: time.Now(),
+		sender: address,
 	}
-	blocks := make(map[string]Block)
-	sender := `"Sender":` + `"` + address + `"`
-
+	
+	// Open log file and read line by line to exract node data
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
+		// Get the start time of each round and if save it in case we propose a block 
+		// we can get the end time in the RoundConcluded step and calculate the block time
+		if (strings.Contains(line, "ProposalAssembled")) {
+			parsedData.startTime = ProposalAssembledParser(&line)
+			continue
+		}
+
+				// Check and see if the a vote we broadcasted is a soft vote
+		if strings.Contains(line, "VoteBroadcast") && strings.Contains(line, "\"ObjectStep\":1") {
+			SoftVotesParser(&line, &parsedData)
+			continue
+		}
+
+		// Check and see if the a vote we broadcasted is a cert vote
+		if strings.Contains(line, "VoteBroadcast") && strings.Contains(line, "\"ObjectStep\":2") {
+			CertVotesParser(&line, &parsedData)
+			continue
+		}
+
+		// Check if we have any blocks we have proposed
 		if (strings.Contains(line, "ProposalBroadcast")) {
-			var data map[string]interface{}
-			json.Unmarshal([]byte(line), &data)
-			
-			
-			// get round and date time
-			var round = fmt.Sprintf("%.0f", data["Round"])
-			var dateTime = fmt.Sprintf("%s", data["time"])
-
-			// Add to the map
-			blocks[round] = Block{block: round, isOnChain: false, dateTime: dateTime}
-			
-			// increment
-			counters.blocksProposed++
+			ProposalBroadcastParser(&line, &parsedData)
 			continue
 		}
 
-		if (strings.Contains(line, "RoundConcluded") && strings.Contains(line, sender)) {
-			var data map[string]interface{}
-			json.Unmarshal([]byte(line), &data)
-			var round = fmt.Sprintf("%.0f", data["Round"])
-			fmt.Println(round)
-			
-			// Find block and update isOnChain
-			block := blocks[round]
-			block.isOnChain = true
-			blocks[round] = block
-
-			// increment
-			counters.blocksOnChain++
+		// Check if the RoundConcluded is any blocks we have proposed from the ProposalBroadcast step 
+		// and get the block time and if our node proposed that block on chain
+		if strings.Contains(line, "RoundConcluded")  && strings.Contains(line, fmt.Sprintf("%d", parsedData.round)) {
+			RoundConcludedParser(&line, &parsedData)
 			continue
 		}
 
-		// Search for Soft and Cert votes
-		if (strings.Contains(line, "VoteBroadcast") && strings.Contains(line, "\"ObjectStep\":1")) {
-			counters.softVotes++
-			continue
-		}
-		
-		if (strings.Contains(line, "VoteBroadcast") && strings.Contains(line, "\"ObjectStep\":2")) {
-			counters.certVotes++
-			continue
-		}
+
 	}
 	
-	file.Close()
+	defer file.Close()
 
-	// Print results
-	fmt.Println(counters)
-	for _, e := range blocks {
-		fmt.Println(e)
+	fmt.Println(parsedData.totals)
+
+	blockArr := blockSorter(parsedData.Blocks)
+	for _, block := range blockArr {
+		fmt.Println(block)
 	}
+
+	bl := parsedData.Blocks[40557166]
+	blockTime := durationToSeconds(bl.endTime, bl.startTime)
+
+	fmt.Printf("Block Time for Round 40557166: %s\n", bl.endTime.Sub(bl.startTime).String())
+	fmt.Printf("Block Time for Round 40557166: %0.2f\n", blockTime)
+}
+
+// Helper function to convert duration to seconds
+func durationToSeconds(et time.Time, st time.Time) float64 {
+	blockTimeStr := strings.Split(et.Sub(st).String(), "s")[0]
+	blockTime, err := strconv.ParseFloat(blockTimeStr, 64)
+
+	if err != nil {
+		panic(err)
+	}
+	return blockTime
+}
+
+func ProposalAssembledParser(line *string) time.Time {
+	parsedJson := Blocks{}
+	jsonErr := json.Unmarshal([]byte(*line), &parsedJson)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+	
+	// Save the start time of each round, and save for later if needed
+	startTime, err := time.Parse(time.RFC3339Nano, parsedJson.TimeStamp)
+	if err != nil {
+		panic(err)
+	}
+	return startTime
+}
+
+func ProposalBroadcastParser(line *string, ld *LogData) {
+	// extract json log data from line
+	parsedJson := Blocks{}
+	jsonErr := json.Unmarshal([]byte(*line), &parsedJson)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+
+	// Add to the map
+	ld.Blocks[parsedJson.Round] = parsedJson
+
+	// Save this round in order to extrac block time end time in RoundConcluded
+	ld.round = uint64(parsedJson.Round)
+
+	// increment total
+	ld.totals.blocksProposed++
+}
+
+func RoundConcludedParser(line *string, ld *LogData) {
+	// Check see if this round is in the block map
+	if _, ok := ld.Blocks[ld.round]; ok {
+		parsedJson := Blocks{}
+		jsonErr := json.Unmarshal([]byte(*line), &parsedJson)
+		if jsonErr != nil {
+			panic(jsonErr)
+		}
+
+		// Find block and update isOnChain
+		block := ld.Blocks[parsedJson.Round]
+		block.startTime = ld.startTime
+		block.endTime, _ = time.Parse(time.RFC3339Nano, parsedJson.TimeStamp)
+
+		// Check and see if the sender is our account, if so set isOnChain to true
+		if parsedJson.Sender == ld.sender {
+			block.isOnChain = true
+			ld.totals.blocksOnChain++
+		}
+		ld.Blocks[parsedJson.Round] = block
+	}
+}
+
+func SoftVotesParser(line *string, ld *LogData) {
+	parsedJson := Votes{}
+	jsonErr := json.Unmarshal([]byte(*line), &parsedJson)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+
+	ld.votes = append(ld.votes, parsedJson)
+	ld.totals.softVotes++
+}
+
+func CertVotesParser(line *string, ld *LogData) {
+	parsedJson := Votes{}
+	jsonErr := json.Unmarshal([]byte(*line), &parsedJson)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+
+	ld.votes = append(ld.votes, parsedJson)
+	ld.totals.certVotes++
+}
+
+
+func blockSorter(b map[uint64]Blocks) []Blocks {
+	keys := make([]string, 0, len(b))
+	for k := range b {
+		keys = append(keys, fmt.Sprintf("%d", k))
+	}
+
+	sort.Strings(keys)
+	blocks := []Blocks{}
+
+	for _, k := range keys {
+		round, err := strconv.ParseUint(k, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		blocks = append(blocks, b[round])
+	}
+	return blocks
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/rs/zerolog v1.33.0
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 )
