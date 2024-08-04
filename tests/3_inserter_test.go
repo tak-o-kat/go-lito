d5947fc47bae269a370aaa699c69adf766936ccd
takokat <danobaba@protonmail.com>
2024-08-04 09:19:20 -0700

    Update inserter tests with new returned map type

--- tests/3_inserter_test.go
+++ tests/3_inserter_test.go
@@ -6,6 +6,7 @@ import (
 	"go-lito/internal/misc"
 	"go-lito/internal/parser"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,15 +20,17 @@ func TestInserterTotals(t *testing.T) {
 	path += "test/"
 	file := "test.db"
 
-	err := os.MkdirAll(path, 0777); if err != nil {
-		t.Errorf("%s",err)
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
 	}
 
 	db := database.New(logger, path, file)
 	db.CreateTables()
 
 	expected := true
-	got := db.CheckDefaultTables(); if expected != got {
+	got := db.CheckDefaultTables()
+	if expected != got {
 		t.Errorf("expected %v; got %v", expected, got)
 	}
 
@@ -47,14 +50,22 @@ func TestInserterTotals(t *testing.T) {
 		err := db.InsertTotals(totals)
 		assert.NoError(t, err)
 
-		dbTotals := db.GetAllTotals()
+		mapTotals := db.GetAllTotals()
+
+		// Convert map to struct
+		dbTotals := new(parser.Totals)
+		dbTotals.SoftVotes, _ = strconv.Atoi((*mapTotals)["soft"]["count"])
+		dbTotals.CertVotes, _ = strconv.Atoi((*mapTotals)["cert"]["count"])
+		dbTotals.BlocksOnChain, _ = strconv.Atoi((*mapTotals)["onChain"]["count"])
+		dbTotals.BlocksProposed, _ = strconv.Atoi((*mapTotals)["proposed"]["count"])
+
 		assert.Equal(t, *totals, *dbTotals)
 	})
 
 	t.Run("Test Insert Votes", func(t *testing.T) {
 
 		votes := getMockVotes()
-		
+
 		err := db.InsertVotes(votes)
 		assert.NoError(t, err)
 
@@ -65,68 +76,68 @@ func TestInserterTotals(t *testing.T) {
 	t.Run("Test Insert Proposals", func(t *testing.T) {
 
 		proposals := getMockProposals()
-		
+
 		err := db.InsertProposals(proposals)
 		assert.NoError(t, err)
 
 		dbProposals := db.GetProposals(len(*proposals))
 		// logger.Debug().Msgf("%v", *dbProposals)
 		assert.Equal(t, *proposals, *dbProposals)
-	})	
+	})
 }
 
 func getMockVotes() *[]parser.Votes {
 	votes := new([]parser.Votes)
 	votes = &[]parser.Votes{
 		{
-			Round: 2481766,
+			Round:     2481766,
 			TimeStamp: "2024-07-27T17:31:45.816675-07:00",
-			Type: 1,
+			Type:      1,
 		},
 		{
-			Round: 2457651,
+			Round:     2457651,
 			TimeStamp: "2024-07-27T19:44:44.591018-07:00",
-			Type: 2,
+			Type:      2,
 		},
 		{
-			Round: 2424465,
+			Round:     2424465,
 			TimeStamp: "2024-07-27T16:53:38.073434-07:00",
-			Type: 2,
+			Type:      2,
 		},
 		{
-			Round: 2487006,
+			Round:     2487006,
 			TimeStamp: "2024-07-27T13:55:09.222409-07:00",
-			Type: 2,
+			Type:      2,
 		},
 		{
-			Round: 2454329,
+			Round:     2454329,
 			TimeStamp: "2024-07-27T09:28:39.134837-07:00",
-			Type: 1,
+			Type:      1,
 		},
 		{
-			Round: 2401004,
+			Round:     2401004,
 			TimeStamp: "2024-07-28T02:03:01.971322-07:00",
-			Type: 1,
+			Type:      1,
 		},
 		{
-			Round: 2473691,
+			Round:     2473691,
 			TimeStamp: "2024-07-27T15:31:27.157012-07:00",
-			Type: 2,
+			Type:      2,
 		},
 		{
-			Round: 2444607,
+			Round:     2444607,
 			TimeStamp: "2024-07-27T15:38:53.390045-07:00",
-			Type: 2,
+			Type:      2,
 		},
 		{
-			Round: 2409687,
+			Round:     2409687,
 			TimeStamp: "2024-07-28T03:08:26.367853-07:00",
-			Type: 1,
+			Type:      1,
 		},
 		{
-			Round: 2492179,
+			Round:     2492179,
 			TimeStamp: "2024-07-28T03:08:26.367853-07:00",
-			Type: 1,
+			Type:      1,
 		},
 	}
 	return votes
@@ -136,73 +147,73 @@ func getMockProposals() *[]parser.Blocks {
 	proposals := new([]parser.Blocks)
 	proposals = &[]parser.Blocks{
 		{
-			Round: 2481766,
+			Round:     2481766,
 			TimeStamp: "2024-07-27T17:31:45.816675-07:00",
 			IsOnChain: true,
-			TypeId: 4,
+			TypeId:    4,
 			BlockTime: 2.8475,
 		},
 		{
-			Round: 2457651,
+			Round:     2457651,
 			TimeStamp: "2024-07-27T19:44:44.591018-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.8891,
 		},
 		{
-			Round: 2424465,
+			Round:     2424465,
 			TimeStamp: "2024-07-27T16:53:38.073434-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.9645,
 		},
 		{
-			Round: 2487006,
+			Round:     2487006,
 			TimeStamp: "2024-07-27T13:55:09.222409-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.8942,
 		},
 		{
-			Round: 2454329,
+			Round:     2454329,
 			TimeStamp: "2024-07-27T09:28:39.134837-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.8447,
 		},
 		{
-			Round: 2401004,
+			Round:     2401004,
 			TimeStamp: "2024-07-28T02:03:01.971322-07:00",
 			IsOnChain: true,
-			TypeId: 4,
+			TypeId:    4,
 			BlockTime: 2.8186,
 		},
 		{
-			Round: 2473691,
+			Round:     2473691,
 			TimeStamp: "2024-07-27T15:31:27.157012-07:00",
 			IsOnChain: false,
-			TypeId: 3,
-			BlockTime: 2.9075,	
+			TypeId:    3,
+			BlockTime: 2.9075,
 		},
 		{
-			Round: 2444607,
+			Round:     2444607,
 			TimeStamp: "2024-07-27T15:38:53.390045-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.8306,
 		},
 		{
-			Round: 2409687,
+			Round:     2409687,
 			TimeStamp: "2024-07-28T03:08:26.367853-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.7932,
 		},
 		{
-			Round: 2492179,
+			Round:     2492179,
 			TimeStamp: "2024-07-28T03:08:49.367853-07:00",
 			IsOnChain: false,
-			TypeId: 3,
+			TypeId:    3,
 			BlockTime: 2.8888,
 		},
 	}
