94df8f99d9f57e39d18f8318ec331e5dad948a09
takokat <danobaba@protonmail.com>
2024-08-04 18:25:39 -0700

    Add in 3 new route for votes (soft and cert)

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -23,24 +23,16 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	r.HandlerFunc(http.MethodGet, "/health", s.healthHandler)
 
-	r.HandlerFunc(http.MethodGet, "/api/hello", s.HelloWorldHandler)
-
+	// Routes for /api/totals
 	r.HandlerFunc(http.MethodGet, "/api/totals/", s.totalsHandler)
-	r.GET("/api/total/:id", s.totalHandler)
+	r.GET("/api/totals/:id", s.totalsIdHandler)
+
+	// TODO: Add routes for /api/votes and /api/votes/:id
+	r.GET("/api/votes/:limit", s.votesHandler)         // returns num votes determined by limit
+	r.GET("/api/votes/:id", s.votesIdHandler)          // return 1 vote by it's id
+	r.GET("/api/votes/type/:id", s.votesTypeIdHandler) // return votes by type (SOFT or CERT)
 
 	return r
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -63,7 +55,7 @@ func (s *Server) totalsHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(jsonResp)
 }
 
-func (s *Server) totalHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (s *Server) totalsIdHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	id := ps.ByName("id")
 	if id == "" {
 		http.Error(w, "Missing id parameter", http.StatusBadRequest)
@@ -79,6 +71,36 @@ func (s *Server) totalHandler(w http.ResponseWriter, r *http.Request, ps httprou
 	// TODO: Add condition to check if id is valid less than or equal to 5
 
 	jsonResp, err := json.Marshal(s.db.GetTotalFor(idInt))
+
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) votesHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	jsonResp, err := json.Marshal(s.db.GetAllTotals())
+
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) votesIdHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	jsonResp, err := json.Marshal(s.db.GetAllTotals())
+
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) votesTypeIdHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	jsonResp, err := json.Marshal(s.db.GetAllTotals())
 
 	if err != nil {
 		log.Fatalf("error handling JSON marshal. Err: %v", err)
