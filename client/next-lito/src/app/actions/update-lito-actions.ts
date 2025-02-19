fdf7652c40aab406d541125081c4dda7174e2692
tak-o-kat <takokat.algo@gmail.com>
2025-02-18 16:31:17 -0800

    another test

--- client/next-lito/src/app/actions/update-lito-actions.ts
+++ client/next-lito/src/app/actions/update-lito-actions.ts
@@ -8,9 +8,7 @@ import { promisify } from "util";
 const execAsync = promisify(exec);
 
 export async function updateLito(_prevState: any, formData: FormData) {
-  const updateCommand = `sudo systemctl stop lito.service && sudo systemctl stop next-lito.service && cd $LITO_FOLDER && \ 
-      curl -s https://api.github.com/repos/tak-o-kat/go-lito/releases/latest | grep "browser_download_url.*update" | cut -d : -f 2,3 | tr -d \\" | wget -qO- -i - | tar xjf - && \ 
-      sudo systemctl start lito.service && sudo systemctl start next-lito.service`;
+  const updateCommand = `sudo systemctl stop next-lito.service && sudo systemctl start next-lito.service`;
 
   const session = await getSession();
   if (!session) {
