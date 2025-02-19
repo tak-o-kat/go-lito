414968bf0d4e55dea5ee7cdb5dd823815be3afa5
tak-o-kat <takokat.algo@gmail.com>
2025-02-18 16:23:37 -0800

    More testing with updates

--- client/next-lito/src/app/actions/update-lito-actions.ts
+++ client/next-lito/src/app/actions/update-lito-actions.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { getSession } from "@/lib/auth/session";
-import { revalidatePath } from "next/cache";
 
 import { exec } from "child_process";
 import { promisify } from "util";
@@ -9,9 +8,9 @@ import { promisify } from "util";
 const execAsync = promisify(exec);
 
 export async function updateLito(_prevState: any, formData: FormData) {
-  const updateCommand = `sudo systemctl stop lito.service && cd $LITO_FOLDER && \ 
+  const updateCommand = `sudo systemctl stop lito.service && sudo systemctl stop next-lito.service && cd $LITO_FOLDER && \ 
       curl -s https://api.github.com/repos/tak-o-kat/go-lito/releases/latest | grep "browser_download_url.*update" | cut -d : -f 2,3 | tr -d \\" | wget -qO- -i - | tar xjf - && \ 
-      sudo systemctl start lito.service`;
+      sudo systemctl start lito.service && sudo systemctl start next-lito.service`;
 
   const session = await getSession();
   if (!session) {
@@ -20,15 +19,9 @@ export async function updateLito(_prevState: any, formData: FormData) {
     };
   }
   try {
-    const { stdout, stderr } = await execAsync(updateCommand);
-    if (stderr) {
-      throw new Error(stderr);
-    }
-    console.log({ success: true, output: stdout });
+    execAsync(updateCommand);
+    return { success: true, message: "Update command sent successfully" };
   } catch (error: any) {
-    console.log({ success: false, error: error.message });
+    return { success: false, error: error.message };
   }
-
-  // save theme in session
-  revalidatePath("/", "layout");
 }
